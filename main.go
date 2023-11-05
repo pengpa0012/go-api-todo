2ae1b45461f2fabffbc93cc7903398bc9356b544
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-05 09:17:21 +0800

    added routes

--- main.go
+++ main.go
@@ -24,8 +24,25 @@ func getTodos(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func deleteTodo(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func toggleTodo(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
 func main() {
 	router := gin.Default()
+
 	router.GET("/todos", getTodos)
+	router.POST("/addTodo", addTodo)
+	router.POST("/deleteTodo", deleteTodo)
+	router.PATCH("/toggleTodo", toggleTodo)
+
 	router.Run("localhost:8080")
 }
